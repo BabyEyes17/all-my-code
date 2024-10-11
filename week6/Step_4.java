1d1fa7fbb8a1bc1bf220e7b4090a59f3dc1eac0c
Jayden Lewis <147560938+BabyEyes17@users.noreply.github.com>
2024-10-11 10:56:51 -0400

    Delete week6 directory

--- week6/Step_4.java
+++
@@ -1,9 +0,0 @@
-//Modify the Bird class to override the eat() method to print "The bird pecks at food.".
-//Use super.eat() inside the Bird's eat() method to call the superclass version before printing  the new message.
-
-public class Step_4 {
-    public static void main(String[] args) {
-        
-    }
-    
-}
